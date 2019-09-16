e3392a90c01e072ef37ac2041f9fe92b94398ac3
bereket11 <bhaile@ucsc.edu>
2019-09-16 03:10:27 -0700

    SecCommit

---
+++ CommonTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using k_love.Controllers;
+using k_love.Models;
+
+
+
+namespace Tests
+{
+    public class Tests
+    {
+        LoveContext _loveContext;
+        
+        //[SetUp]
+        //public void Setup()
+        //{
+        //}
+        public Tests()
+        {
+            //loveContext = new LoveContext();
+        }
+
+        //[Fact]
+        //public async Task IndexReturnsDeptsViewResult()
+        //{
+        //    var mockRepo = new Mock.
+        //    Assert.Equals
+        //}
+
+        //[Test]
+        //public void TestDepts()
+        //{
+        //    HomeController dept = new HomeController();
+        //    IAction testAction = dept.Index();
+        //    Assert.IsInstanceOf<Department>(dept);
+
+            
+        //   // Assert.Pass();
+        }
+    }
+}
